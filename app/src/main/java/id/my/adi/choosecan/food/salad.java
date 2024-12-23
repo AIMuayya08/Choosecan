aaf968d5edf9d755c35f659be4b09330094111cb
AIMuayya08 <144983188+AIMuayya08@users.noreply.github.com>
2024-12-23 21:58:46 +0700

    intens
    codingan

--- app/src/main/java/id/my/adi/choosecan/food/salad.java
+++ app/src/main/java/id/my/adi/choosecan/food/salad.java
@@ -1,7 +1,6 @@
 package id.my.adi.choosecan.food;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,13 +9,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import id.my.adi.choosecan.R;
 
-public class    salad extends AppCompatActivity {
+public class salad extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Enable Edge-to-Edge
         EdgeToEdge.enable(this);
+
+        // Set layout for Salad activity
         setContentView(R.layout.activity_salad2);
+
+        // Apply window insets listener for padding adjustments
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,3 +29,6 @@ public class    salad extends AppCompatActivity {
         });
     }
 }
+
+
+
--- app/src/main/java/id/my/adi/choosecan/main/MainActivity.java
+++ app/src/main/java/id/my/adi/choosecan/main/MainActivity.java
@@ -1,14 +1,16 @@
 package id.my.adi.choosecan.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import id.my.adi.choosecan.R;
+import id.my.adi.choosecan.food.salad;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Adjust padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Navigate to SaladActivity when the button is clicked
+        findViewById(R.id.btnCekResepSalad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, salad.class);
+                startActivity(intent);
+            }
+        });
     }
 }
